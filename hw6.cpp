46670323f79d6f9796e1a07b5e9d04bc35abd64d
YammiDummy <50939736+YammiDummy@users.noreply.github.com>
2021-05-27 17:58:47 +0500

    Add files via upload

---
+++ hw6.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <algorithm>
+
+using namespace std;
+mutex m;
+
+template <typename T>
+void pcout(T& temp) {               
+    m.lock();
+    cout << temp;
+    m.unlock();
+}
+
+bool PrimeCheck (int n) {
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;    // must be prime
+}
+
+void GetPrime(int index) {
+    lock_guard lg(m);
+    int current = 2;
+    for (int count = 0; count < index;) {
+        if (PrimeCheck(current)) {++count;}
+        ++current;
+        cout << "Number checking: " << current << endl;
+    }
+    cout << "Prime number #" << index << " is " << --current;
+}
+
+int main()
+{
+    int a = 5;
+    string s = "Carrot";
+    thread t1([](auto temp) {pcout(temp); }, a);
+    thread t2([](auto temp) {pcout(temp); }, s);
+    t1.join();
+    t2.join();
+
+    thread t3(GetPrime, 500);
+    t3.join();
+};
