9b62f5c4c15b46109a5ef21117ac41db63587374
YammiDummy <50939736+YammiDummy@users.noreply.github.com>
2021-06-07 14:19:15 +0500

    Update hw7.cpp

--- hw7.cpp
+++ hw7.cpp
@@ -1,1 +1,23 @@
 #include <iostream>
+
+
+//School.proto
+//syntax = "proto 3";
+//
+//package school;
+//
+//message FullName {
+//	string name = 1;
+//	string surname = 2;
+//	string patronymic = 3;
+//	}
+//
+//message Student {
+//	FullName full_name = 1;
+//	repeated int32 grades = 2;
+//	int32 average_grade = 3;
+//	}
+//
+//message StudentsGroup {
+//	repeated Student students = 1;
+//	}
