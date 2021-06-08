e10452457b2f3ddf6c66569842a73656b6bb2773
YammiDummy <50939736+YammiDummy@users.noreply.github.com>
2021-06-08 23:47:03 +0500

    Update hw7.cpp

--- hw7.cpp
+++ hw7.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-
+#include <fstream>
+#include <optional>
+#include <algorithm>
+#include "car.pb.h"
 
 //School.proto
 //syntax = "proto 3";
@@ -21,3 +24,56 @@
 //message StudentsGroup {
 //	repeated Student students = 1;
 //	}
+
+
+
+using namespace std;
+
+class IRepository {
+    virtual void Open() = 0; // бинарная десериализация в файл
+    virtual void Save() = 0; // бинарная сериализация в файл
+};
+
+class IMethods {
+    virtual double GetAverageScore(const FullName& name) = 0;
+    virtual string GetAllInfo(const FullName& name) = 0;
+    virtual string GetAllInfo() = 0;
+};
+
+class FullName {
+private:
+    string m_name;
+    string m_surname;
+    optional<string> m_patronymic;
+public:
+    FullName(string name, string surname, optional<string> patronymic = nullopt)
+        :m_name(name), m_surname(surname), m_patronymic(patronymic) {};
+    FullName() {};
+
+    bool operator== (FullName other) {
+        return tie(m_name, m_surname, m_patronymic) == tie(other.m_name, other.m_surname, other.m_patronymic);
+    };
+};
+
+class Student {
+private:
+    FullName full_name;
+    vector<int> grades; 
+};
+
+class StudentsGroup : public IRepository, IMethods {
+private:
+    vector<Student> group;
+public:
+    double GetAverageScore(const FullName& name) override{
+        for( Student student : group) {
+
+        }
+    };
+    string GetAllInfo(const FullName& name) override {};
+    string GetAllInfo() override {};
+};
+
+int main() {
+
+}
